d486f950b07a7a5ca567ffe38460a17a9df13f43
Marina <marinaporfirio11@gmail.com>
2023-07-28 12:28:27 -0300

    Css

---
+++ AmandaViagens/AmandaViagens/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AmandaViagens.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
