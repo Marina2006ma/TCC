92b1faa42f9bab71f3da783cdc34706aa5527f1a
Marina <marinaporfirio11@gmail.com>
2023-09-22 12:18:16 -0300

    Página Sobre nós

--- AmandaViagens/AmandaViagens/wwwroot/js/blog_custom.js
+++ AmandaViagens/AmandaViagens/wwwroot/js/blog_custom.js
@@ -43,6 +43,7 @@ $(document).ready(function()
 	initMenu();
 	initLightbox();
 	initSearchForm();
+	initTestSlider();
 
 	/* 
 
@@ -191,6 +192,57 @@ $(document).ready(function()
 					searchActive = false;
 				}
 			});	
+		}
+	}
+
+	/* 
+
+	7. Init Testimonials Slider
+
+	*/
+
+	function initTestSlider()
+	{
+		if($('.test_slider').length)
+		{
+			var testSlider = $('.test_slider');
+
+			testSlider.owlCarousel(
+			{
+				loop:true,
+				nav:false,
+				dots:false,
+				smartSpeed:1200,
+				margin:30,
+				responsive:
+				{
+					0:{items:1},
+					480:{items:1},
+					768:{items:2},
+					992:{items:3}
+				}
+			});
+
+			/* Custom nav events */
+			if($('.test_slider_prev').length)
+			{
+				var prev = $('.test_slider_prev');
+
+				prev.on('click', function()
+				{
+					testSlider.trigger('prev.owl.carousel');
+				});
+			}
+
+			if($('.test_slider_next').length)
+			{
+				var next = $('.test_slider_next');
+
+				next.on('click', function()
+				{
+					testSlider.trigger('next.owl.carousel');
+				});
+			}
 		}
 	}
 });
