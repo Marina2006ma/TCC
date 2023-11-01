115ae3c2806a9b1fa4f65d69b97d7431e18bd300
Marina <marinaporfirio11@gmail.com>
2023-11-01 12:28:25 -0300

    Criação de Classes

---
+++ AmandaViagens/AmandaViagens/Models/Cruzeiro.cs
@@ -0,0 +1,29 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace AmandaViagens.Models;
+
+    [Table("Cruzeiro")]
+    public class Cruzeiro
+    
+    {
+        public int Id { get; set; }
+       
+        [Required(ErrorMessage = "Informe o Nome")]
+        [StringLength(80, ErrorMessage = "O Nome deve possuir no máximo 80 caracteres")]
+        public string Nome { get; set; }
+
+        [Display(Name = "Descrição")]
+        [StringLength(2000, ErrorMessage = "A Descrição  deve possuir no máximo 2000 caracteres")]
+        public string Descricao { get; set; }
+
+        [Display(Name = "Preço")]
+        [Column(TypeName = "decimal(8,2)")]
+        public decimal? Preco { get; set; }
+        
+        [StringLength(200)]
+        [Display(Name = "Foto")]
+        public string Image { get; set; }
+
+
+    }
